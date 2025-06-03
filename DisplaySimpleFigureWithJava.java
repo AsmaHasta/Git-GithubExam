2e08fc5b9249be2e5ff80536540c8326eec2b938
Asma <a.movahedy82@gmail.com>
2025-06-03 10:39:17 +0330

    add a code to previus code

--- DisplaySimpleFigureWithJava.java
+++ DisplaySimpleFigureWithJava.java
@@ -11,6 +11,35 @@ public class DisplaySimpleFigureWithJava {
     }
 }
 
+public class DisplaySimpleFigureWithJava {
+    public static void main(String[] args) {
+        stars();
+        System.out.println();
+        stars();
+        ninety();
+        stars();
+        System.out.println();
+        stars();
+        fourStar();
+        ninety();
+        ninety();
+        fourStar();
+        fourStar();
+    }
 
+    public static void ninety() {
+        System.out.println("* | | | | *");
+    }
+
+    public static void stars() {
+        System.out.println("  *****  ");
+        System.out.println(" ******** ");
+        System.out.println("************");
+    }
+
+    public static void fourStar() {
+        System.out.println("  *****  ");
+    }
+}
 
 
